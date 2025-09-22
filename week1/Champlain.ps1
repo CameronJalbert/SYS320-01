8aeb51b63b10c0528c2066a5789037975d901666
CameronJalbert <cameron.jalbert@mymail.champlain.edu>
2025-09-22 19:42:37 -0400

    Changes to be committed: 	new file:   week1/Champlain.ps1 	new file:   week1/Exportstoppedfiles.ps1 	new file:   week1/GetProcessNameC.ps1 	new file:   week1/NoSys32.ps1 	new file:   week1/StoppedServices.csv

---
+++ week1/Champlain.ps1
@@ -0,0 +1,9 @@
+﻿$par = Get-Process | Where-Object { $_.ProcessName -eq "chrome" }
+
+if ($par) {
+    Get-Process | Where-Object { $_.ProcessName -eq "chrome" } | Stop-Process -Force
+    Write-Host "Chrome was running and has been stopped."
+} else {
+    Start-Process "chrome.exe" "https://www.champlain.edu"
+    Write-Host "Chrome was not running. Started Chrome and navigated to Champlain.edu"
+}
