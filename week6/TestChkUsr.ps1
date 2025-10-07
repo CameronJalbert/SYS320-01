184de5af9ae9828d5c8a011c753a9fdfef9384ec
CameronJalbert <cameron.jalbert@mymail.champlain.edu>
2025-10-07 01:03:12 -0400

    Local User Management Menu scripts 	new file:   week6/Event-Logs.ps1 	new file:   week6/String-Helper.ps1 	new file:   week6/TestChkUsr.ps1 	new file:   week6/Users.ps1 	new file:   week6/main-1.ps1 	new file:   week6/passchkr.ps1 	new file:   week6/testscript2.ps1

---
+++ week6/TestChkUsr.ps1
@@ -0,0 +1,5 @@
+﻿
+. (Join-Path $PSScriptRoot Users.ps1)   # dot-source to load functions
+
+checkUser "NoSuchUser123"     # should return False
+checkUser $env:USERNAME       # should return True (local user)
